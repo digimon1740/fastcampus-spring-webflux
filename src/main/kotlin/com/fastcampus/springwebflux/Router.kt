56a0771b63bd7d10651219f6a69bfdea4cb20767
sanghoon <digimon.1740@gmail.com>
2022-07-03 00:02:35 +0900

    수정

---
+++ src/main/kotlin/com/fastcampus/springwebflux/Router.kt
@@ -0,0 +1,29 @@
+package com.fastcampus.springwebflux
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
+
+@Configuration
+class Router {
+
+    @Bean
+    fun helloRouter(handler: HelloHandler): RouterFunction<ServerResponse> =
+        route()
+            .GET("/", handler::sayHello)
+            .build()
+
+
+    @Bean
+    fun userRouter(handler: UserHandler): RouterFunction<ServerResponse> =
+        router {
+            "/users".nest {
+                GET("/users/{id}", handler::getUser)
+                GET("/users", handler::getAll)
+            }
+        }
+
+}
---
+++ src/main/kotlin/com/fastcampus/springwebflux/UserHandler.kt
@@ -0,0 +1,28 @@
+package com.fastcampus.springwebflux
+
+
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
+
+class User(val id: Long, val email: String)
+
+@Component
+class UserHandler {
+
+    val users = listOf(
+        User(id = 1, email = "user1@gmail.com"),
+        User(id = 2, email = "user2@gmail.com")
+    )
+
+    fun getUser(request: ServerRequest): Mono<ServerResponse> =
+        users.find { request.pathVariable("id").toLong() == it.id }
+            ?.let {
+                ServerResponse.ok().bodyValue(it)
+            } ?: ServerResponse.notFound().build()
+
+    fun getAll(request: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok().bodyValue(users)
+
+}
